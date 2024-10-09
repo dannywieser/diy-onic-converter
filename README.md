a51cca9f342c369cf21250a7cbffb05113f8c2cd
Danny Wieser <danny.wieser@gmail.com>
2024-10-09 10:30:04 -0600

    bootstrap: basic functionality and create rough task list

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Start by reading the challenge instructions in [INTERVIEW.md](./INTERVIEW.md). W
 _or…_
 
 **Sta**rt **b**y **read**ing **th**e **challen**ge **instructio**ns **i**n **[INTERVIEW.](./INTERVIEW.md)**[md](./INTERVIEW.md). **Whe**n **yo**u **ar**e **finish**ed, **yo**u **ca**n **edi**t **thi**s **fil**e **t**o **inclu**de **an**y **documentat**ion **fo**r **you**r **wor**k.
+
+## TODO
+
+* [ ] Get all p tags on the page and just list them out
+* [ ] Parse the words in those p tags into an array
+* [ ] Figure out how to identify which portions of the word to highlight
+* [ ] Figure out how to highlight those words
+* [ ] Test on real world examples
+* [ ] Document usage
--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -1,11 +1,10 @@
-/**
- * Implement your converter function here.
- */
-const diyOnicConverter = (textContentContainerSelector) => {
-  const container = document.querySelector(textContentContainerSelector);
-  console.log("Performing bionic reading conversion on:", container);
 
-  /* Etc. etc. etc. your code etc. */
+const diyOnicConverter = (textContentContainerSelector) => {
+  const conversionTargetTag = "p"
+  const container = document.querySelector(textContentContainerSelector);
+  // TODO: iterate over array of candidate tags vs single
+  const conversionElements =  container.getElementsByTagName(conversionTargetTag)
+  console.log(`Performing bionic reading conversion on: ${textContentContainerSelector}, located ${conversionElements.length} elements to process`);
 };
 
 // Allow global access so that this can be executed from the console.
